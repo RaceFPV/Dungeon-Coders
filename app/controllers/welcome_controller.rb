326be2047c74ab75abc22df6e37310f8cc90db25
slacker87 <racethesunlive@yahoo.com>
2014-12-06 09:08:52 +0000

    add a global variable "players" that saves the list of players in game"

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
+  @@players = []
+
   def index
+    @@players += [current_or_guest_player.email.gsub(/[^0-9A-Za-z]/, '')]
+    @players = @@players
   end
 end
