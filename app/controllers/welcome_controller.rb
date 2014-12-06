5a99ac8748c8df6af14e80443b28011ab21ffb4d
slacker87 <racethesunlive@yahoo.com>
2014-12-06 10:20:44 +0000

    add messaging and other routes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,17 @@ class WelcomeController < ApplicationController
   @@players = []
 
   def index
+    @messages = Message.all.order('created_at DESC').limit(50)
     @@players += [current_or_guest_player.email.gsub(/[^0-9A-Za-z]/, '')]
     @players = @@players
+  end
+
+  def new_message
+    @message = Message.new
+    @message.content = params[:message][:content]
+    @message.name = current_or_guest_player.email
+    @message.player_id = current_or_guest_player.id
+    @message.save!
+    return render :nothing => true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
+  get '/chat' => 'multiplayer#chat'
+  get '/movement' => 'multiplayer#movement'
+  get '/joined' => 'multiplayer#joined'
   devise_for :players, controllers: { sessions: "players/sessions" }
+  match '/message/new', to: 'welcome#new_message', via: 'post', as: 'new_message'
 end
