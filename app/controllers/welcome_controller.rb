4680c7221151e0887945ec5cfcc04120914e658b
alexander taylor <a@alexandertaylor.org>
2014-12-06 21:45:17 +0000

    Devise fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,23 +1,19 @@
 class WelcomeController < ApplicationController
+  
   @@players = []
 
   def index
     @messages = Message.all.order('created_at DESC').limit(50)
-    if current_player
-      @@players += [current_player.email.gsub(/[^0-9A-Za-z]/, '')]
-      @thisplayer = current_player.email.gsub(/[^0-9A-Za-z]/, '')
-    else
-      @@players += [guest_player.email.gsub(/[^0-9A-Za-z]/, '')]
-      @thisplayer = guest_player.email.gsub(/[^0-9A-Za-z]/, '')
-    end
+    @@players += [get_player.email.gsub(/[^0-9A-Za-z]/, '')]
     @players = @@players
+    @thisplayer = get_player.email.gsub(/[^0-9A-Za-z]/, '')
   end
 
   def new_message
     @message = Message.new
     @message.content = params[:message][:content]
-    @message.name = current_or_guest_player.email
-    @message.player_id = current_or_guest_player.id
+    @message.name = get_player.email
+    @message.player_id = get_player.id
     @message.save!
     return render :nothing => true
   end
