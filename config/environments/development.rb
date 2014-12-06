34b4319747f2a60d0e92c49c543fd8607f5bd385
slacker87 <racethesunlive@yahoo.com>
2014-12-06 18:22:48 +0000

    add a puma config file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.cache_classes = false
   config.eager_load = false
   config.middleware.delete Rack::Lock
-
+  
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
