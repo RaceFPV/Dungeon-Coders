38e466b3ee2fb9fa963973211c0e40c372c38bfe
slacker87 <racethesunlive@yahoo.com>
2014-12-06 20:33:36 +0000

    fix alexes stuff

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,18 +2,17 @@ class WelcomeController < ApplicationController
   @@players = []
 
   def index
-    random = Random.rand(10000000...99999999)
     @messages = Message.all.order('created_at DESC').limit(50)
-    @@players += player_signed_in? ? [current_player.email.gsub(/[^0-9A-Za-z]/, '')] : ["guest-#{random}"]
+    @@players += [current_or_guest_player.email.gsub(/[^0-9A-Za-z]/, '')]
     @players = @@players
-    @thisplayer = player_signed_in? ? [current_player.email.gsub(/[^0-9A-Za-z]/, '')] : ["guest-#{random}"]
+    @thisplayer = current_or_guest_player.email.gsub(/[^0-9A-Za-z]/, '')
   end
 
   def new_message
     @message = Message.new
     @message.content = params[:message][:content]
-    @message.name = current_player.email
-    @message.player_id = current_player.id
+    @message.name = current_or_guest_player.email
+    @message.player_id = current_or_guest_player.id
     @message.save!
     return render :nothing => true
   end
