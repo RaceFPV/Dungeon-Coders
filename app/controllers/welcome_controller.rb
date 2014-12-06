3157ee80d9bee95a96e70dcaffbc3cb0c592f3e1
alexander taylor <a@alexandertaylor.org>
2014-12-06 20:22:11 +0000

    Devise login fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,17 +2,18 @@ class WelcomeController < ApplicationController
   @@players = []
 
   def index
+    random = Random.rand(10000000...99999999)
     @messages = Message.all.order('created_at DESC').limit(50)
+    @@players += player_signed_in? ? [current_player.email.gsub(/[^0-9A-Za-z]/, '')] : ["guest-#{random}"]
     @players = @@players
-    @@players += [current_or_guest_player.email.gsub(/[^0-9A-Za-z]/, '')]
-    @thisplayer = current_or_guest_player.email.gsub(/[^0-9A-Za-z]/, '')
+    @thisplayer = player_signed_in? ? [current_player.email.gsub(/[^0-9A-Za-z]/, '')] : ["guest-#{random}"]
   end
 
   def new_message
     @message = Message.new
     @message.content = params[:message][:content]
-    @message.name = current_or_guest_player.email
-    @message.player_id = current_or_guest_player.id
+    @message.name = current_player.email
+    @message.player_id = current_player.id
     @message.save!
     return render :nothing => true
   end
